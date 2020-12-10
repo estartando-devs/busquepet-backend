0f2f0c969c67e82fd1a571bbfcabef60019f7ce4
Alan Araujo <alan@estartandodevs.com.br>
2020-12-09 23:09:21 -0300

    fix: pet schema

--- functions/src/interfaces/pet.ts
+++ functions/src/interfaces/pet.ts
@@ -1,16 +1,19 @@
-import { ILocation } from "./adress";
+import { ILocation } from "./address";
 
-type Species = "cat" | "dog"
-type Genre = "male" | "female"
-type Size = "short" | "middle" | "great"
+type Species = "cat" | "dog";
+type Genre = "male" | "female";
+type Size = "short" | "middle" | "great";
 
 export interface IPet {
-  name: string
-  specie: Species
-  genre: Genre
-  age: string
-  images?: string[]
-  description?: string
-  size?: Size
-  location?: ILocation
+  name: string;
+  specie: Species;
+  genre: Genre;
+  age: string;
+  size?: Size;
+  location?: ILocation;
+  imageUrl?: string;
+  donor?: string;
+  adopter?: string;
+  description?: string;
+  vaccination?: string;
 }
